9d1b5980fb5714d4c8eced7d66e631565597ad1e
kkya <evnscent@gmail.com>
2026-06-25 23:07:09 +0700

    Delete backend-api directory

--- backend-api/app/Config/Routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use CodeIgniter\Router\RouteCollection;
-
-/** @var RouteCollection $routes */
-$routes->get('/', 'Home::index');
-$routes->group('api', function($routes) {
-    $routes->post('login', 'Auth::login');
-});
